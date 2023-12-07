63e80dca9073b63110993ed216f62bfdb7cbf4e6
Animila <khristoforov-i@mail.ru>
2023-12-07 09:24:11 +0900

    модалка

--- backend/API/controllers/areasController.js
+++ backend/API/controllers/areasController.js
@@ -4,21 +4,39 @@ const prisma = new PrismaClient()
 const AreasController = {
 	getAll: async () => {
 		var data = null
+		var owners = null
 		try {
-			const result = await prisma.area.findMany()
-			var newResult = []
-			result.map(item => {
-				newResult.push({
-					id: item.id,
-					coordinates: [JSON.parse(item.coordinates)],
-				})
-			})
-			console.log('3')
+			const resultArea = await prisma.area.findMany()
 
-			console.log('1', newResult)
+			const newResult = await Promise.all(
+				resultArea.map(async item => {
+					const ownersIds = await prisma.ownerArea.findMany({
+						where: {
+							areaId: item.id,
+						},
+						select: {
+							ownerId: true,
+						},
+					})
+					if (ownersIds[0] && ownersIds[0].ownerId) {
+						owners = await prisma.owner.findFirst({
+							where: {
+								id: ownersIds[0].ownerId,
+							},
+						})
+					}
+
+					return {
+						id: item.id,
+						coordinates: [JSON.parse(item.coordinates)],
+						owners,
+					}
+				})
+			)
+
 			data = { success: true, data: newResult }
 		} catch (e) {
-			data = { success: false, message: e.message || 'Unknown error' }
+			data = { success: false, message: e.message || 'Неизвестная ошибка' }
 		}
 		return data
 	},
--- frontend/src/components/Map/MapContainer.jsx
+++ frontend/src/components/Map/MapContainer.jsx
@@ -4,6 +4,7 @@ import { Map, Polygon, YMaps } from '@pbe/react-yandex-maps'
 import React, { useEffect, useState } from 'react'
 const MapContainer = () => {
 	const [dataMap, setDataMap] = useState([])
+	const [selectedItem, setSelectedItem] = useState(null)
 
 	const getAllAreas = async () => {
 		const data = await (await AreasServices.getAll()).data
@@ -14,6 +15,56 @@ const MapContainer = () => {
 	useEffect(() => {
 		getAllAreas()
 	}, [])
+
+	const openPopup = (event, item) => {
+		setSelectedItem(item)
+		if (!item.owners) {
+			return
+		}
+
+		// Вычислите позицию всплывающего окна относительно клика
+		const x = event.get('clientX')
+		const y = event.get('clientY')
+
+		// Создайте всплывающее окно
+		const popup = document.createElement('div')
+		popup.className = 'custom-popup'
+		popup.style.position = 'absolute'
+		popup.style.left = `${x}px`
+		popup.style.top = `${y}px`
+		popup.style.background = 'white'
+		popup.onclick = () => {
+			closePopup()
+			// Добавьте другие действия, которые нужно выполнить при нажатии на всплывающее окно
+			console.log('Popup clicked!')
+		}
+
+		console.log(item.owners.ownerData)
+
+		// Заполните всплывающее окно данными
+		popup.innerHTML = `
+      <h4>Информация о владельце</h4>
+      ${
+				item.owners &&
+				`<div>
+              <p>ФИО: ${item.owners.ownerData}</p>
+          </div>`
+			}
+    `
+
+		// Добавьте всплывающее окно на карту
+		document.body.appendChild(popup)
+	}
+
+	const closePopup = () => {
+		setSelectedItem(null)
+
+		// Удалите всплывающее окно из DOM
+		const popup = document.querySelector('.custom-popup')
+		if (popup) {
+			popup.remove()
+		}
+	}
 
 	return (
 		<YMaps
@@ -32,9 +83,13 @@ const MapContainer = () => {
 				{dataMap.map((item, index) => (
 					<React.Fragment key={index}>
 						<Polygon
+							options={{
+								fillColor: item.owners ? '#B22222' : '#4169E1',
+							}}
 							geometry={item.coordinates.map(coords =>
 								coords.map(coord => [coord[1], coord[0]])
 							)}
+							onClick={event => openPopup(event, item)}
 						/>
 					</React.Fragment>
 				))}
